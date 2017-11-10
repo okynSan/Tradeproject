4d6aad2828aac4734dac15b4ac86f667a1374588
Andrey Zatvornitskiy <okyn.san@gmail.com>
2017-11-10 23:22:50 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/ua/anzaevsk/util/Validator.java

---
+++ src/main/java/ua/anzaevsk/util/valildators/optionsvalidator/OptionsValidatorImpl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.anzaevsk.util.valildators.optionsvalidator;
+
+import ua.anzaevsk.model.trade.Options;
+
+public class OptionsValidatorImpl implements IOptionsValidator {
+
+    @Override
+    public String validateOptionsTrade(Options trade) {
+
+        if (trade.getStyle().equals("American") || trade.getStyle().equals("European")) {
+            if (trade.getStyle().equals("American") && (trade.getExcerciseStartDate().after(trade.getTradeDate())
+                    && trade.getExcerciseStartDate().before(trade.getExpiryDate()))) {
+                if (trade.getExpiryDate().before(trade.getDeliveryDate())
+                        && trade.getPremiumDate().before(trade.getDeliveryDate())) {
+                        return "Validation passed";
+                }
+
+            }
+        }
+        return "Validation wasn't passed";
+    }
+
+}
